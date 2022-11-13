322ee16ad6bf7288f1fdf94bea5d8057cedb7224
Matheus <hmatheus00@outlook.com>
2022-11-13 00:15:27 +0000

    Criação do script para subir aplicação com o apache

---
+++ iac.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+
+apt update -y
+apt upgrade -y
+
+apt install apache2 -y
+apt install unzip -y
+
+cd /tmp
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+
+cd linux-site-dio
+
+cp -R * /var/www/html
